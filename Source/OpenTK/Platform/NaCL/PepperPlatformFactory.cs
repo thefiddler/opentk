079b3ba455824665d09c96be5680b697830f9486
thefiddler <stapostol@gmail.com>
2014-07-08 17:27:52 +0200

    [NACL] Added stub implementation

---
+++ Source/OpenTK/Platform/NaCL/PepperPlatformFactory.cs
@@ -0,0 +1,78 @@
+﻿#region License
+//
+// PepperPlatformFactory.cs
+//
+// Author:
+//       Stefanos A. <stapostol@gmail.com>
+//
+// Copyright (c) 2006-2014 Stefanos Apostolopoulos
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
+
+using System;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace OpenTK.Platform.NaCL
+{
+    class PepperPlatformFactory : PlatformFactoryBase
+    {
+        #region IPlatformFactory implementation
+
+        public override INativeWindow CreateNativeWindow(int x, int y, int width, int height, string title, GraphicsMode mode, GameWindowFlags options, DisplayDevice device, int major, int minor, GraphicsContextFlags flags)
+        {
+            return new PepperNativeWindow(x, y, width, height, title, mode, options, device, major, minor, flags);
+        }
+
+        public override IDisplayDeviceDriver CreateDisplayDeviceDriver()
+        {
+            return new PepperDisplayDriver();
+        }
+
+        public override IGraphicsContext CreateGLContext(GraphicsMode mode, IWindowInfo window, IGraphicsContext shareContext, bool directRendering, int major, int minor, GraphicsContextFlags flags)
+        {
+            return new PepperGraphicsContext(mode, window, shareContext, directRendering, major, minor, flags);
+        }
+
+        public override GraphicsContext.GetCurrentContextDelegate CreateGetCurrentGraphicsContext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IKeyboardDriver2 CreateKeyboardDriver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IMouseDriver2 CreateMouseDriver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IJoystickDriver2 CreateJoystickDriver()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
+
